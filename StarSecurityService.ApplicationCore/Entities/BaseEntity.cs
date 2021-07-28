6f24113b71d9728d5dbc5b62defef215ea8fda6d
ADMIN <ADMIN@DESKTOP-6S60LF4>
2021-07-28 08:52:49 +0700

    Add project files.

---
+++ StarSecurityService.ApplicationCore/Entities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarSecurityService.ApplicationCore.Entities
+{
+    public abstract class BaseEntity
+    {
+        public virtual int Id { get; protected set; }
+    }
+}
---
+++ StarSecurityService.ApplicationCore/Entities/Employyee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarSecurityService.ApplicationCore.Entities
+{
+    public class Employyee : BaseEntity
+    {
+        public string Code{ get; set; }
+        public string Name { get; set; }
+        public string Adress{ get; set; }
+        public int ContactNumber{ get; set; }
+        public int? Role { get; set; }
+        public bool Grade { get; set; }
+        public int Client { get; set; }
+        public int? Achievements { get; set; }
+        public DateTime BirthDay { get; set; }
+      
+    }
+}
