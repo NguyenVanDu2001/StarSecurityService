a0d5142a64062c207217cb95904f3ce8737996b3
DuNV <nguyenduc1908i1bkap@gmail.com>
2021-08-01 21:29:09 +0700

    add controller and view granted  permisstion

--- StarSecurityService.Web/Areas/Admin/Controllers/BussinessController.cs
+++ StarSecurityService.Web/Areas/Admin/Controllers/BussinessController.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using StarSecurityService.ApplicationCore.Entities;
+using StarSecurityService.ApplicationCore.InterFaces;
+using StarSecurityService.EntityFramework.Data;
+using StarSecurityService.Web.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,9 +14,53 @@ namespace StarSecurityService.Web.Areas.Admin.Controllers
     public class BussinessController : Controller
     {
         // GET: Admin/Bussiness
-        public ActionResult Index()
+        private readonly IAsyncRepository<Bussiness> _bussinessReponsitory;
+        private readonly IAsyncRepository<Permission> _permisstionReponsitory;
+        public BussinessController()
         {
-            return View();
+            _bussinessReponsitory = new EfRepository<Bussiness>(new StarServiceDbContext());
+            _permisstionReponsitory = new EfRepository<Permission>(new StarServiceDbContext());
+        }
+
+        // GET: Bussiness
+        public async Task<ActionResult> Index()
+        {
+            return View(await _bussinessReponsitory.ListAllAsync());
+        }
+        public async Task<ActionResult> Update()
+        {
+            var controller = GetControllerHelper.GetController("StarSecurityService.Web.Controllers");
+            // insert controller
+            foreach (var ctrl in controller)
+            {
+                Bussiness b = new Bussiness
+                {
+                    Id = ctrl.Name,
+                    Name = ctrl.Name,
+                    Status = true
+                };
+                if (!(await _bussinessReponsitory.GetAll()).Any(x => x.Id.Equals(b.Id)))
+                {
+                    await _bussinessReponsitory.AddAsync(b);
+                }
+                // Get Permisssons thuộc Bussiness current
+                var actions = GetControllerHelper.GetActions(ctrl);
+                foreach (var act in actions)
+                {
+                    Permission p = new Permission
+                    {
+                        Id = ctrl.Name + "-" + act, // CategorController-Index
+                        Name = ctrl.Name,
+                        BusinessId = ctrl.Name,
+                        Status = false
+                    };
+                    if (!(await _permisstionReponsitory.GetAll()).Any(x => x.Id.Equals(p.Id)))
+                    {
+                        await _permisstionReponsitory.AddAsync(p);
+                    }
+                }
+            }
+            return RedirectToAction("Index");
         }
     }
 }
