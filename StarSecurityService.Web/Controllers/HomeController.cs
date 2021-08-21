168c9fbeaaa8264bef18531271e57a223af57062
DuNV <nguyenduc1908i1bkap@gmail.com>
2021-08-21 15:58:31 +0700

    Hoanf thanh about us

--- StarSecurityService.Web/Controllers/HomeController.cs
+++ StarSecurityService.Web/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using StarSecurityService.Application.Branchs;
 using StarSecurityService.Application.Clients;
+using StarSecurityService.Application.Histories;
 using StarSecurityService.Application.ServiceOffers;
 using StarSecurityService.EntityFramework.Data;
 using StarSecurityService.Web.Helpers;
+using StarSecurityService.Web.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,22 +20,28 @@ namespace StarSecurityService.Web.Controllers
         private readonly IClientAppService _clientAppServices;
         private readonly IServiceOfferService _serviceOfferService;
         private readonly IBrachAppService _branchService;
-
+        private readonly IHistoryService _historyService;
+        private readonly IShareHolderService _shareHolderService;
         public HomeController()
         {
             db = new StarServiceDbContext();
             _clientAppServices = new ClientAppServices();
             _serviceOfferService = new ServiceOfferService();
             _branchService = new BranchAppService();
+            _historyService = new HistoryService();
+            _shareHolderService = new ShareHolderService();
         }
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult AboutUs()
-        { 
-            return View();
+        public async Task<ActionResult> AboutUs()
+        {
+            var model = new HomeAboutUs();
+            model.HistoryModel = await _historyService.GetAll();
+            model.ShareHolderModel = await _shareHolderService.GetAll();
+            return View(model);
         }
         public ActionResult Career()
         {
